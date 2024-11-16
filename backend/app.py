47f4e1107abb003486a96f2250d910286183f7ef
Jutin Joco <justin.joco@gmail.com>
2024-11-15 23:25:11 -0800

    changes postgres container name

--- backend/app.py
+++ backend/app.py
@@ -15,7 +15,7 @@ def create_app():
     app = Flask(__name__)
     CORS(app)
     app.config["SQLALCHEMY_DATABASE_URI"
-               ] = "postgresql://admin:password@postgres_db/url_db"
+               ] = "postgresql://admin:password@postgres/url_db"
     # initialize the app with the extension
     db.init_app(app)
     ## Initialize Config
